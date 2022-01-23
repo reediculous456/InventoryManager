0b7dcbfd1492760e1a07f7cfb327b2306343aabe
Wesley Reed <reed.wesley.s@gmail.com>
2022-01-22 23:41:05 -0500

    Initial commit

---
+++ src/test/java/edu/uc/inventorymanager/InventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uc.inventorymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
