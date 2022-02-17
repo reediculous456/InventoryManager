830c20459a039012ab03defeec7cc0aafc24098c
Wesley Reed <reed.wesley.s@gmail.com>
2022-02-16 19:45:48 -0500

    oops, interfaces not classes

--- src/main/java/edu/uc/inventorymanager/service/IItemService.java
+++ src/main/java/edu/uc/inventorymanager/service/IItemService.java
@@ -1,4 +1,15 @@
 package edu.uc.inventorymanager.service;
 
-public class IItemService {
+import edu.uc.inventorymanager.dto.Item;
+
+import java.util.List;
+
+public interface IItemService {
+    void save(Item item);
+
+    List<Item> fetchALl();
+
+    Item fetch(int id);
+
+    void assign(int userId);
 }
--- src/main/java/edu/uc/inventorymanager/service/IUserService.java
+++ src/main/java/edu/uc/inventorymanager/service/IUserService.java
@@ -1,4 +1,13 @@
 package edu.uc.inventorymanager.service;
 
-public class IUserService {
+import edu.uc.inventorymanager.dto.User;
+
+import java.util.List;
+
+public interface IUserService {
+    void save(User user);
+
+    List<User> fetchALl();
+
+    User fetch(int id);
 }
