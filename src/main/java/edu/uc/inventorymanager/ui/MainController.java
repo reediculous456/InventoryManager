9098395ff9a0493afd6803d6fd2df96779be4972
Wesley Reed <reed.wesley.s@gmail.com>
2022-02-16 19:36:27 -0500

    added base UI

--- src/main/java/edu/uc/inventorymanager/ui/MainController.java
+++ src/main/java/edu/uc/inventorymanager/ui/MainController.java
@@ -1,2 +1,16 @@
-package edu.uc.inventorymanager.ui;public class MainController {
+package edu.uc.inventorymanager.ui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    /**
+     * Handle the / endpoint
+     * @return
+     */
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
 }
