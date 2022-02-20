74cab21c38399671e6d7af3e78622802ab5b0fd1
salingmw <58449342+salingmw@users.noreply.github.com>
2022-02-20 00:08:45 -0500

    Added fetch by id for ItemStatus

---
+++ src/main/java/edu/uc/inventorymanager/service/IItemStatusService.java
@@ -0,0 +1,15 @@
+package edu.uc.inventorymanager.service;
+
+import edu.uc.inventorymanager.dto.ItemStatus;
+
+import java.util.List;
+
+public interface IItemStatusService {
+
+    void save(ItemStatus itemStatus);
+
+    List<ItemStatus> fetchALl();
+
+
+    ItemStatus fetchItemStatusById(int i);
+}
---
+++ src/test/java/edu/uc/inventorymanager/InventoryManagerApplicationItemStatusTests.java
@@ -0,0 +1,42 @@
+package edu.uc.inventorymanager;
+
+import edu.uc.inventorymanager.dto.ItemStatus;
+import edu.uc.inventorymanager.dto.User;
+import edu.uc.inventorymanager.service.IItemStatusService;
+import edu.uc.inventorymanager.service.IUserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class InventoryManagerApplicationItemStatusTests {
+
+    @Autowired
+    IItemStatusService itemStatusService;
+    private ItemStatus itemStatus;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void fetchItemStatusById_returnsItemStatusId5(){
+        givenItemStatusDataAreAvailable();
+        whenSearchItemStatusWithId5();
+        thenReturnItemStatusForId5();
+    }
+
+    private void whenSearchItemStatusWithId5() {
+        itemStatus = itemStatusService.fetchItemStatusById(5);
+    }
+
+    private void givenItemStatusDataAreAvailable() {
+    }
+
+    private void thenReturnItemStatusForId5() {
+        String name = itemStatus.getName();
+        assertEquals("Test Name", name);
+    }
+}
