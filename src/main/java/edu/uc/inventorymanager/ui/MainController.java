fd93db714dae900b2ed648eea33f9dfdb638c7fd
Daniel Cullen <34354477+CullenDaniel@users.noreply.github.com>
2022-02-20 04:15:31 -0500

    Added DB file, and item

--- src/main/java/edu/uc/inventorymanager/ui/MainController.java
+++ src/main/java/edu/uc/inventorymanager/ui/MainController.java
@@ -1,6 +1,9 @@
 package edu.uc.inventorymanager.ui;
 
+import edu.uc.inventorymanager.dto.Item;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +13,18 @@ public class MainController {
      * @return
      */
     @RequestMapping("/")
+    public String index(Model model){
+        Item item = new Item();
+        item.setId(0);
+        item.setName("Name");
+        item.setDescription("Des");
+        item.setLocation("locale");
+        item.setAssigned_to(1);
+        item.setStatus_id(0);
+        model.addAttribute(item);
+        return "index";
+    }
+    @GetMapping("/item")
     public String index() {
         return "index";
     }
