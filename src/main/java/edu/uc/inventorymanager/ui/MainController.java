930185953b1e5f417db37efc079fe9058aeb3dcc
Daniel Cullen <34354477+CullenDaniel@users.noreply.github.com>
2022-02-20 04:45:31 -0500

    Added IIteamDAO and the Frame to write to Database

--- src/main/java/edu/uc/inventorymanager/ui/MainController.java
+++ src/main/java/edu/uc/inventorymanager/ui/MainController.java
@@ -4,7 +4,9 @@ import edu.uc.inventorymanager.dto.Item;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -16,7 +18,7 @@ public class MainController {
     public String index(Model model){
         Item item = new Item();
         item.setId(0);
-        item.setName("Name");
+        item.setName("item");
         item.setDescription("Des");
         item.setLocation("locale");
         item.setAssigned_to(1);
@@ -24,6 +26,23 @@ public class MainController {
         model.addAttribute(item);
         return "index";
     }
+    @RequestMapping("saveItem")
+    public String saveItem(Item item){
+        //itemService.save(item);
+        return "index";
+    }
+    /*
+    @PostMapping(value ="/item", consumes="application/json", produces = "application/json")
+    @ResponseBody
+    public Item createItem(@ResponseBody Item item){
+        Item newItem = null;
+        try {
+            //newItem = itemService.save(item);
+        } catch (Exception e){
+            //
+        }
+    }
+     */
     @GetMapping("/item")
     public String index() {
         return "index";
