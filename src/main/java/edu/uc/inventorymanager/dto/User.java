3fdec00f8858fa8b482678ede109ed54e3d40b8f
salingmw <58449342+salingmw@users.noreply.github.com>
2022-03-09 19:07:55 -0500

    SerializedName issues

--- src/main/java/edu/uc/inventorymanager/dto/User.java
+++ src/main/java/edu/uc/inventorymanager/dto/User.java
@@ -4,5 +4,32 @@ import lombok.Data;
 
 public @Data class User {
     private int id;
-    private String name;
+    private String username;
+
+    @SerializedName("gender")
+    private String gender;
+    @SerializedName("name")
+    private Name name;
+    @SerializedName("location")
+    private Location location;
+    @SerializedName("email")
+    private String email;
+    @SerializedName("login")
+    private Login login;
+    @SerializedName("dob")
+    private Dob dob;
+    @SerializedName("registered")
+    private Registered registered;
+    @SerializedName("phone")
+    private String phone;
+    @SerializedName("cell")
+    private String cell;
+    @SerializedName("id")
+    private Id id;
+    @SerializedName("picture")
+    private Picture picture;
+    @SerializedName("nat")
+    private String nat;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 }
